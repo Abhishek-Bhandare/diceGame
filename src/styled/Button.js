9e303c702f03e8b904232f0ee45b24dd72c37370
Abhishek-Bhandare <abhishekbhandare1@gmail.com>
2024-04-18 01:47:14 +0530

    first commit

---
+++ src/styled/Button.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+padding: 10px 18px;
+min-width: 220px;
+background: #000000;
+border-radius: 5px;
+color: white;
+font-size: 16px;
+cursor: pointer; 
+
+&:hover{
+  background-color: #383838;
+}
+`;
